cb7f3c070f7bd160a0a49ac9729d6816138906ae
cjayet <c.jayet@optimiz.me>
2016-12-20 17:33:52 +0100

    first commit

---
+++ Passerelle/Block/Getproductslist.php
@@ -0,0 +1,28 @@
+<?php
+namespace Optimizmeformagento\Passerelle\Block;
+
+class Getproductslist extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Get all products
+     */
+    public function getProductsList() {
+
+        $tabProducts = array();
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+        $collection = $productCollection->create()
+            ->addAttributeToSelect('*')
+            ->load();
+
+        foreach ($collection as $product){
+            print_r($product);
+            echo 'Name  =  '.$product->getName().'<br />';
+        }
+
+        die;
+    }
+
+
+}
---
+++ Passerelle/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Optimizmeformagento_Passerelle',
+    __DIR__
+);
